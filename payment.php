e327edf9e7c29a7e11aebc20ff24dc3c3287da75
krishna1278 <86706770+krishna1278@users.noreply.github.com>
2021-06-30 12:03:02 +0530

    Add files via upload

---
+++ payment.php
@@ -0,0 +1,9 @@
+<div align="center" style="padding:20px;">
+<h2>Payment Option for You</h2>
+<b>Pay with</b><a href="http://www.paypal.com"><img src="images/PayPal.jpg" width="250" height="120"></a> <b> Or <a href="order.php">Pay Offline</a></b> <br>
+<b> If You Selected "Pay Offline" option then please check your email or account to find the Product Details  for your Order</b>
+</div>
+
+
+
+
